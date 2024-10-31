e265e63a3db40fc6f91e504a68da7734bd99ee9c
João Pedro Carmo <142856277+joaopcarmo@users.noreply.github.com>
2024-10-30 23:31:26 -0300

    adicionando script.js
    adicionado script para preencher cards

---
+++ src/script.js
@@ -0,0 +1,23 @@
+function renderizarCachorros() {
+    const container = document.querySelector('.pets-section');
+    let html = '';
+
+    // biome-ignore lint/complexity/noForEach: <explanation>
+    cachorros.forEach(cachorro => {
+        html += `
+            <div class="pet-card">
+                <img src="${cachorro.foto}" alt="${cachorro.nome}">
+                <h3>${cachorro.nome}</h3>
+                <p>${cachorro.raça}</p>
+                <p>${cachorro.sexo}</p>
+                <p>${cachorro.descrição}</p>
+                <p>${cachorro.idade}</p>
+                <a href="#" class="button">Adote</a>
+            </div>
+        `;
+    });
+
+    container.innerHTML = html;
+}
+
+document.addEventListener('DOMContentLoaded', renderizarCachorros);
